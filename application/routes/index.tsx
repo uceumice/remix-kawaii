1af8afa908ca13a6324d3491190a5d8d75aa860e
uceumice <hi@uceumice.com>
2023-02-08 07:08:42 +0100

    Replaced unnecessary links from landing page.

--- application/routes/index.tsx
+++ application/routes/index.tsx
@@ -10,26 +10,13 @@ export default function Index() {
       ])}
     >
       <div className={clsx(['bg-fill w-fit h-fit', 'p-5', 'rounded-xl'])}>
-        <h1>Welcome to Remix</h1>
-        <ul>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/blog"
-              rel="noreferrer"
-            >
-              15m Quickstart Blog Tutorial
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/jokes"
-              rel="noreferrer"
-            >
-              Deep Dive Jokes App Tutorial
-            </a>
-          </li>
+        <h1>Welcome to Remix Kawaii</h1>
+        <h4>
+          Right now there is not much to see here. But don't worry, it has only
+          been {((new Date().valueOf() / 1000 - 1675835511) / 3600).toFixed(1)}{' '}
+          Hours since last change.
+        </h4>
+        <ul className={clsx(['flex flex-col justify-between'])}>
           <li>
             <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
               Remix Docs
