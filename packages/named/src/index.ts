519740e70af0f2c968d818eb2d026910a97c8760
uceumice <hi@uceumice.com>
2023-03-18 08:12:02 +0100

    minor formatting

--- packages/named/src/index.ts
+++ packages/named/src/index.ts
@@ -1,5 +1,5 @@
-export { getNamed as init } from './get-named';
+export { getNamed as init } from "./get-named";
 
 // ----
-export { getActionNameFromURL as InURL } from './get-action-name-from-url';
-export { getActionNameFromFormData as InFormData } from './get-action-name-from-form-data';
+export { getActionNameFromURL as InURL } from "./get-action-name-from-url";
+export { getActionNameFromFormData as InFormData } from "./get-action-name-from-form-data";
