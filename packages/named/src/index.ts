51fe57841643238eff615c98f06911b4edf7223b
uceumice <hi@uceumice.com>
2023-04-16 22:58:18 +0200

    export NamedActiions type

--- packages/named/src/index.ts
+++ packages/named/src/index.ts
@@ -3,3 +3,6 @@ export { getNamed as init } from "./get-named";
 // ----
 export { getActionNameFromURL as InURL } from "./get-action-name-from-url";
 export { getActionNameFromFormData as InFormData } from "./get-action-name-from-form-data";
+
+// ----
+export type { NamedActions } from "./get-named";
